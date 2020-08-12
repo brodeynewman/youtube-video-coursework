e4fdb5d571871d23017f079011e8de77b99baf68
Brodey Newman <brodeynewman@gmail.com>
2020-08-12 16:30:44 -0400

    examples from video

--- fp/pure.js
+++ fp/pure.js
@@ -1,22 +1,67 @@
-// pure function
-const thisIsPure = num => num * 2;
+/* -------- EXAMPLE ONE --------- */
 
-// not pure
-let num = 5;
-const thisIsNotPure = () => num *= 2;
-thisIsNotPure();
 
-console.log(num);
+// let array = ['there', 'Hello'];
 
-// next
-let arr = ['there', 'hello'];
-const alsoNotPure = arr => arr.reverse().join(' ');
-const result = alsoNotPure(arr);
+// const reverseArray = () => array.reverse().join(' ');
 
-console.log(arr, result);
+// console.log(reverseArray());
+// console.log(array);
 
-let arrTwo = ['there', 'hello'];
-const thisIsNowPure = arr => [...arr].reverse().join(' ');
-const resultTwo = thisIsNowPure(arrTwo);
 
-console.log(arrTwo, resultTwo);
+
+/* -------- EXAMPLE TWO --------- */
+
+
+// let array = ['there', 'Hello'];
+
+// const reverseArray = arr => [...arr].reverse().join(' ');
+
+// console.log(reverseArray(array));
+// console.log(array);
+
+/* -------- EXAMPLE THREE --------- */
+
+
+// const updateUser = (user) => {
+//   const updatedUser = { ...user };
+
+//   updatedUser.age = 25;
+//   updatedUser.color = 'green';
+
+//   return updatedUser;
+// }
+
+/* -------- EXAMPLE FOUR --------- */
+
+
+// const updateUser = (user) => {
+//   user.age = 25;
+//   user.color = 'green';
+
+//   return user;
+// };
+
+// const initialUser = {};
+
+// const result = updateUser(initialUser);
+
+// console.log(result, initialUser);
+
+/* -------- EXAMPLE FIVE --------- */
+
+
+const updateUser = (user) => {
+  const updatedUser = { ...user };
+  
+  updatedUser.age = 25;
+  updatedUser.color = 'green';
+
+  return updatedUser;
+};
+
+const initialUser = {};
+
+const result = updateUser(initialUser);
+
+console.log(result, initialUser);
